8554e1f1324ba839841ed18d761ecbe96f3fd63f
TigranPetrosyants <tigran.petrosyants.fd@gmail.com>
2023-05-01 13:33:13 +0400

    Fix PUT method

--- books.js
+++ books.js
@@ -23,29 +23,43 @@ app.get('/:id', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    const newBook = {
-        id: Math.floor((Math.random() + 1) * 100000),
-        title: req.body.title,
-        author: req.body.author
-    };
-    books.push(newBook);
-    fs.writeFile("books.json", JSON.stringify(books), (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-    res.status(200).json(newBook)
+    if (req.body.title && req.body.author) {
+        const newBook = {
+            id: Math.floor((Math.random() + 1) * 100000),
+            title: req.body.title,
+            author: req.body.author
+        };
+        books.push(newBook);
+        fs.writeFile("books.json", JSON.stringify(books), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        res.status(201).json(newBook);
+    }else {
+        res.status(404).send('The Title or Author fields are empty.');
+    }
+
 })
 
 
 app.put('/:id', (req, res) => {
-    const book = books.find((book) => book.id == req.params.id);
-    if (!book) {
+    const bookIndex = books.findIndex((book) => book.id == req.params.id);
+    if (bookIndex  == -1) {
         res.status(404).send('Book not found');
     }else{
-        book.title = req.body.title;
-        book.author = req.body.author;
-        res.status(200).json(book);
+        if (req.body.title && req.body.author) {
+            books[bookIndex].title = req.body.title;
+            books[bookIndex].author = req.body.author;
+            fs.writeFile("books.json", JSON.stringify(books), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            res.status(200).json(books[bookIndex]);
+        }else {
+            res.status(404).send('The Title or Author fields are empty.');
+        }
     }
 
 })
